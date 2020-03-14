478d8f743df07024d8a5f2d52d593ca7e789e75b
sufjanlee <814932470@qq.com>
2020-03-14 22:21:17 +0800

    Module 3 full functions

--- Service.py
+++ Service.py
@@ -33,6 +33,17 @@ redis_host = os.getenv('REDIS_HOST', None)
 redis_password = os.getenv('REDIS_PASSWORD', None)
 redis_port = os.getenv('REDIS_PORT', None)
 
+r = redis.Redis(host=redis_host, password=redis_password, port=redis_port)
+
+# connect the redis server --- Hubert's
+redis_host_li = "redis-19723.c61.us-east-1-3.ec2.cloud.redislabs.com"
+redis_password_li = "15235021453.ljhX"
+redis_port_li = "19723"
+
+r_li = redis.Redis(host=redis_host_li,
+                   password=redis_password_li,
+                   port=redis_port_li)
+
 # obtain the port that heroku assigned to this app.
 heroku_port = os.getenv('PORT', None)
 
@@ -90,6 +101,116 @@ def callback():
             continue
 
     return 'OK'
+
+
+# For Part 3
+flag = 0
+test_x = 0
+test_y = 0
+test_z = 0
+
+
+# Handle self-test function for Part 3
+def basic_measure(event):
+    '''
+    Basic test for user
+    event: user input
+    flag: question No. use as global variable.
+    x, y, z: used for contain user's input, and compute the final score
+    threshold: score threshold
+    '''
+    re_msg = [
+        "Question 1: Have you been to the epidemic area in the past 14 days? 0(No)/1(Yes)",
+        "Question 2: Have you been in contact with a suspected infected person in the past 14 days? 0(No)/1(Yes)",
+        "Qeustion 3: Do you have fever, cough, or difficulty breathing? 0(No)/1(Yes)"
+    ]
+    weight = [0.2, 0.3, 0.5]
+    threshold = 0.7
+    global flag
+    print(flag)
+    global test_x, test_y, test_z
+    # enter the test part for the first time, output the first question
+    if flag == 1:
+        line_bot_api.reply_message(event.reply_token,
+                                   TextSendMessage(re_msg[0]))
+
+    # the second time, recieve user's input -> int. Output the second question
+    if flag == 2:
+        # input value start with $
+        test_x = int(str(event.message.text)[1])
+        print(test_x)
+        if test_x == 0 or test_x == 1:
+            line_bot_api.reply_message(event.reply_token,
+                                       TextSendMessage(re_msg[1]))
+        else:
+            flag = 1
+            print("changed flag when wrong{}".format(flag))
+            test_x, test_y, test_z = 0, 0, 0
+            war_msg = "You input the wrong integer. Please follow the instructions. Thanks!"
+            line_bot_api.reply_message(event.reply_token,
+                                       TextSendMessage(war_msg))
+
+    # the third time, recieve the second answer,.Output the third question
+    if flag == 3:
+        test_y = int(str(event.message.text)[1])
+        if test_y == 0 or test_y == 1:
+            line_bot_api.reply_message(event.reply_token,
+                                       TextSendMessage(re_msg[2]))
+        else:
+            flag = 2
+            test_y, test_z = 0, 0
+            war_msg = "You input the wrong integer. Please follow the instructions. Thanks!"
+            line_bot_api.reply_message(event.reply_token,
+                                       TextSendMessage(war_msg))
+    # the fourth time, recieve the third answer, output the final report.
+    if flag == 4:
+        test_z = int(str(event.message.text)[1])
+        if test_z == 0 or test_z == 1:
+            # calculate the final score
+            score = weight[0] * test_x + weight[1] * test_y + weight[2] * test_z
+            # if the score >= threshold, return high risk
+            if score >= threshold:
+                report = "Based on your description, we suggest you are at higher risk, please go to the hospital for a diagnosis."
+                line_bot_api.reply_message(event.reply_token,
+                                           TextSendMessage(report))
+            # if 0 < score < threshold, return quaratine.
+            elif 0 < score < threshold:
+                report = "Based on your description, we highly suggest that you quarantine for 14 days."
+                line_bot_api.reply_message(event.reply_token,
+                                           TextSendMessage(report))
+            # if score == 0, return healthy
+            else:
+                report = "Based on your description, we think you are healthy, but please wear a mask."
+                line_bot_api.reply_message(event.reply_token,
+                                           TextSendMessage(report))
+        else:
+            flag = 3
+            print("第三个问题错误{}".format(flag))
+            test_z = 0
+            war_msg = "You input the wrong integer. Please follow the instructions. Thanks!"
+            line_bot_api.reply_message(event.reply_token,
+                                       TextSendMessage(war_msg))
+
+
+# Handle normal input
+def handle_Text(event):
+    '''
+    handle Normal questions
+    '''
+    print(event.message.text)
+    msg = event.message.text
+    ls = str(msg).lower().split(" ")
+    print(ls)
+    for i in ls:
+        print(i)
+        # if the key does not exist, return None.
+        value = str(r_li.get(i))
+        if value == "None":
+            continue
+        else:
+            print(value)
+            line_bot_api.reply_message(event.reply_token,
+                                       TextSendMessage(value))
 
 
 # Handler function for Follow Event
@@ -158,8 +279,21 @@ def handle_TextMessage(event):
             event.reply_token,
             TextSendMessage("TO DO... Written by WU Peicong"))
     elif event.message.text.startswith("$"):
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage("TO DO...Written by LI Jinhui"))
+        global flag
+        global test_x, test_y, test_z
+        if event.message.text == "$basic measurements":
+            flag += 1
+            basic_measure(event)
+        else:
+            if flag == 0:
+                handle_Text(event)
+            else:
+                if flag == 4:
+                    # end of the test, reset the global variables
+                    flag, test_x, test_y, test_z = 0, 0, 0, 0
+                else:
+                    flag += 1
+                    basic_measure(event)
     else:
         message = ProcessMessage(event.source.user_id,
                                  event.message.text).public("TextMessage")
