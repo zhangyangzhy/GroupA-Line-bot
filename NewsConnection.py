9af6407c7735073ff369fe68c665d1a722332af9
PatrickNg <patrickngzh@outlook.com>
2020-03-16 00:04:59 +0800

    Add the NewsProvider

--- NewsConnection.py
+++ NewsConnection.py
@@ -1,7 +1,7 @@
 import redis as redis
 
 
-class Connection:
+class NewsConnection:
     def __init__(self):
         self.__host = "redis-17935.c1.ap-southeast-1-1.ec2.cloud.redislabs.com"
         self.__pwd = "h79Y4ieXjngXsPESbT3KAPBVgGa8Hzhs"
