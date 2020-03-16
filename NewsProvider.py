9263e74845d15d28c1091729c5ecc3e59dc05bda
PatrickNg <patrickngzh@outlook.com>
2020-03-16 17:19:31 +0800

    Fix the message

--- NewsProvider.py
+++ NewsProvider.py
@@ -2,11 +2,11 @@ from NewsConnection import NewsConnection
 
 
 class NewsProvider:
-    def __init__(self, userid):
+    def __init__(self, userid, message):
         self.__redis = NewsConnection.connect()
         self.__userid = userid
+        self.__message = message
 
     # get the latest news
     def getNews(self):
-        return self.__userid
-
+        return self.__message+''+self.__userid
--- Service.py
+++ Service.py
@@ -338,7 +338,7 @@ Notice: You should add '$' at the beginning of your query when you want to test 
 # Handler function for Text Message
 def handle_TextMessage(event):
     if str(event.message.text).startswith("@"):
-        message=NewsProvider(event.source.user_id).getNews()
+        message = NewsProvider(event.source.user_id,event.message.text).getNews()
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(message))
