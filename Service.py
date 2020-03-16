d4087c6e7a428b65cca0384c15b70025cce9c488
PatrickNg <patrickngzh@outlook.com>
2020-03-16 17:25:36 +0800

    Fix the message

--- Service.py
+++ Service.py
@@ -339,6 +339,7 @@ Notice: You should add '$' at the beginning of your query when you want to test 
 def handle_TextMessage(event):
     if str(event.message.text).startswith("@"):
         message = NewsProvider(event.source.user_id,event.message.text).getNews()
+        print(message)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(message))
