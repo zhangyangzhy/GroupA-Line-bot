22c70bf829e4bff5e0ea4284461ea35f5b4b73c5
PatrickNg <patrickngzh@outlook.com>
2020-03-16 18:43:28 +0800

    Fix the message

--- WPC/NewsProvider.py
+++ WPC/NewsProvider.py
@@ -3,7 +3,7 @@ from WPC.NewsConnection import NewsConnection
 
 class NewsProvider:
     def __init__(self, userid, message):
-        self.__redis = NewsConnection.connect()
+        self.__redis = NewsConnection().connect()
         self.__userid = userid
         self.__message = message
 
