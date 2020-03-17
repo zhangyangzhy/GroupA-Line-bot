d51cdc471b6219169bec5fd18a60ca72a6716dbf
PatrickNg <patrickngzh@outlook.com>
2020-03-17 23:44:27 +0800

    Fix the @Favourite bug

--- Service.py
+++ Service.py
@@ -336,8 +336,8 @@ Notice: You should add '$' at the beginning of your query when you want to test 
     elif str(event.postback.data).startswith("@Favourite="):
         params = parse.parse_qs(event.postback.data)
         index = params['@Favourite'][0]
-        meg = NewsProvider(event.source.user_id, '@Favourite').handle_message(index)
-
+        message = NewsProvider(event.source.user_id, '@Favourite').handle_message(index)
+        msg = TextSendMessage(message)
     else:
         msg = TextSendMessage("Error")
     line_bot_api.reply_message(event.reply_token, msg)
