a85f1852a45948377003a0e2d3ee2e8473427b48
PatrickNg <patrickngzh@outlook.com>
2020-03-17 09:12:31 +0800

    Add the carousel

--- WPC/NewsProvider.py
+++ WPC/NewsProvider.py
@@ -41,9 +41,78 @@ Across the globe, 47 countries and jurisdictions issued advisories against trave
             titles += str(i + 1) + '. '
             titles += title[i]
             titles += '\n\n\n'
-        return titles
 
-    # handle different type message
+        json = {
+            "type": "template",
+            "altText": "this is a carousel template",
+            "template": {
+                "type": "carousel",
+                "columns": [
+                    {
+                        "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                        "imageBackgroundColor": "#FFFFFF",
+                        "title": "this is menu",
+                        "text": "description",
+                        "defaultAction": {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "http://example.com/page/123"
+                        },
+                        "actions": [
+                            {
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=111"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=111"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/111"
+                            }
+                        ]
+                    },
+                    {
+                        "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                        "imageBackgroundColor": "#000000",
+                        "title": "this is menu",
+                        "text": "description",
+                        "defaultAction": {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "http://example.com/page/222"
+                        },
+                        "actions": [
+                            {
+                                "type": "postback",
+                                "label": "Buy",
+                                "data": "action=buy&itemid=222"
+                            },
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }
+                ],
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover"
+            }
+        }
+        return json
+
+        # handle different type message
+
     def handle_message(self):
         if self.__message == '@News':
             return self.__fetch__news()
