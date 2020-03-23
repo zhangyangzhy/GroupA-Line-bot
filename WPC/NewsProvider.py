1fe56f3aff3e567aeafad29b828b27948480e35b
PatrickNg <patrickngzh@outlook.com>
2020-03-23 21:04:58 +0800

    Fix the news id

--- WPC/NewsProvider.py
+++ WPC/NewsProvider.py
@@ -81,7 +81,9 @@ Across the globe, 47 countries and jurisdictions issued advisories against trave
             # redis.set(self.__userid, self.__news_list[int(index)])
             redis = NewsConnection().connect()
             news = News('Title1', 'Content1', 'Url1')
-            redis.hset(self.__userid, '1', json.dumps(news.__dict__))
+            redis.incr('index')
+            index = redis.get('index')
+            redis.hset(self.__userid, index, json.dumps(news.__dict__))
             return 'Saved Successfully'
         # elif re.match('@Delete \d+$', self.__message) is not None:
         #     return "developing Delete"
